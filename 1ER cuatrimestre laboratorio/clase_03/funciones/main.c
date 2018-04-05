d08a2b378c09ab18c6c4b689dc310cd8e3a2077d
palomomatias <palomomatias@hotmail.com>
2018-04-05 12:25:18 -0300

    falta repetir el error

--- 1ER cuatrimestre laboratorio/clase_03/funciones/main.c
+++ 1ER cuatrimestre laboratorio/clase_03/funciones/main.c
@@ -26,18 +26,83 @@ void saludar(void)desarrolar o implementar(logica) la funcion
 void saludar (saludar);
 int sumar(int numeroUno,int numeroDos);
 float dividirEnteros(int dividiendo,int divisor);
+int pedirNumeroPar();
+
+int getEntero(char* mensaje,char* mensajeError, int maximo, int minimo);
 
 int main()
 {
+int numero;
+numero=getEntero("ingrese su edad","reingrese su edad",199,1);
+printf("el entero ingresado es : %d",numero);
+
+/*
    int numero;
+   numero=getEntero("ingrese su edad",199,1);
+
+   printf("en entero ingresado es : %d",numero);*/
+
+/*
    numero=sumar(3,3);
    printf("%d\n",numero);
    float numero2;
    numero2=dividirEnteros(10,3);
    printf("%f\n",numero2);
 
+   int sePudo;
+   sePudo=pedirNumeroPar();
+
+   if(sePudo==0)
+   {
+    printf("ok");
+
+   }
+   else
+    {
+    printf("mal");
+   }
+
+*/
     return 0;
-}
+}// del main
+
+/** \brief
+ *
+ * \param mensaje char*
+ * \param maximo int
+ * \param minimo int
+ * \return int
+ *
+ */
+
+ int getEntero(char* mensaje,char* mensajeError, int maximo, int minimo)
+ {
+     int entero=-1;
+     do{
+        printf("%s",mensaje);
+        scanf("%d",&entero);
+        if(entero<maximo&&entero>minimo)
+        {
+            break;
+        }
+        else
+        {printf("%s",mensajeError);}
+
+        }while(0);
+        return entero;
+
+
+ }
+
+/*int getEntero(char* mensaje,int maximo, int minimo)
+{
+    int entero;
+    do{
+        printf("%s",mensaje);
+        scanf("%d",&entero);
+    }while(entero< minimo || entero>maximo);
+    return entero;
+}*/
 int sumar(int numeroUno,int numeroDos)
 {
     int resultado=0;
@@ -46,7 +111,25 @@ int sumar(int numeroUno,int numeroDos)
 }
 float dividirEnteros(int dividiendo,int divisor)
 {
-   float resultado2=0;
-   resultado2=(float)dividiendo/divisor;
-   return resultado2;
+   float resultado=0;
+   resultado=(float)dividiendo/divisor;
+   return resultado;
 }
+
+int pedirNumeroPar()
+{
+    int respuesta=-1;
+    int numero;
+    printf("ingrese un numero");
+    scanf("%d",&numero);
+
+    if(numero%2==0 && numero!=0)
+    {
+        respuesta=0;
+    }
+
+    return respuesta;
+
+
+}
+
